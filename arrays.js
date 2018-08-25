cf7bf9aa669efb7300f937e34587fde9818ef47c
Manju Thomas <kudukacar@hotmail.com>
2018-08-25 19:59:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,9 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(Element, Array) {
-  return [Element,...Array]
+  return [Element,...Array];
 }
-function destructivelyAddElementToBeginningOfArray(chocolateBars, element) {
-  return chocolateBars.unshift(element)
+function destructivelyAddElementToBeginningOfArray(Array, Element) {
+  return Array.unshift(Element);
 }
 function addElementToEndOfArray(chocolateBars, element) {
   return [...chocolateBars, element]
